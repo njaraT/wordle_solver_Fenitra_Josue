02139d7072e3450f86f8eec0fba95a1b54239d47
njaraT <njaratianarandriambola4@gamil.com>
2025-04-24 13:30:09 +0400

    MAJ_3

--- include/getBDD.h
+++ include/getBDD.h
@@ -1,18 +1,34 @@
 #ifndef GETBDD_H
 #define GETBDD_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <ctype.h>
+#include <unistd.h>
+
 #define MAX_LINE_LEN 2200
-#define TOTAL_WORDS 2315  // Ajusté pour correspondre au fichier
+#define TOTAL_WORDS 2315
 #define MAX_WORD_LENGTH 6
 
-void generateBDD(char* input_file, char* output_file);
-void getWord(char* bddName, char words[TOTAL_WORDS][MAX_WORD_LENGTH]);
+// Structures de données
+extern char listeMots[TOTAL_WORDS][MAX_WORD_LENGTH];
+extern char guess[MAX_WORD_LENGTH];
+extern int num_try, is_victory, randomIndex;
+extern char restart;
+
+// Prototypes des fonctions
+void toLowerCase(char* str);
+void displayColorLetter(char* word, char* try);
 int checkValidWord(char* word, char words[TOTAL_WORDS][MAX_WORD_LENGTH]);
 void checkLetters(char* word, char* guess, char* try);
-void displayColorLetter(char* word, char* try);
 void filterByIncludedLetters(char words[TOTAL_WORDS][MAX_WORD_LENGTH], char* included, int count);
 void filterByExcludedLetters(char words[TOTAL_WORDS][MAX_WORD_LENGTH], char* excluded, int count);
 void filterByLetterPosition(char words[TOTAL_WORDS][MAX_WORD_LENGTH], char letter, int position);
 void filterBySubstring(char words[TOTAL_WORDS][MAX_WORD_LENGTH], char* substring);
 void displayFilteredWords(char words[TOTAL_WORDS][MAX_WORD_LENGTH]);
+void getWord(char* bddName, char words[TOTAL_WORDS][MAX_WORD_LENGTH]);
+void applyFilters(char originalWords[TOTAL_WORDS][MAX_WORD_LENGTH]);
+
 #endif
